46f1daf2db7a39fa395c6bed9b62400e1079b0b3
Rasheed Johnson <rasheedjohnson@tutanota.com>
2024-03-17 20:10:04 +0200

    Prepared the following: 'properties/[id]/page.jsx', 'properties/add/page.jsx', 'properties/page.jsx', and cleaned up 'app/page.jsx'. From course, completed 'File-Based Routing' video

---
+++ app/properties/page.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const PropertiesPage = () => {
+  return (
+    <div>
+      <h3 className="text-3xl">PropertiesPage</h3>
+      <Link href="/">Home</Link>
+    </div>
+  )
+}
+
+export default PropertiesPage
