9753028a5de2a9d3c3d60228e0bae7a84a53803e
Rasheed Johnson <rasheedjohnson@tutanota.com>
2024-03-25 22:22:25 +0200

    Created following components: 'Footer.jsx', 'Hero.jsx', 'InfoBox.jsx', and 'InfoBoxes.jsx'. Modified 'app/layout.jsx' and 'app/page.jsx'

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,25 +1,25 @@
 import "@/assets/styles/globals.css";
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export const metadata = {
   title: "PropertyPulse | Find The Perfect Rental",
   description: "Find your dream rental property",
   keywords: "rental, find rentals, find properties",
-}
+};
 
-const MainLayout = ({children}) => {
+const MainLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
         <div>
           <Navbar />
-          <main>
-            {children}
-          </main>
+          <main>{children}</main>
+          <Footer />
         </div>
       </body>
     </html>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
